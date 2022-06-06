2521c8100f4282f87c1ce6c13524fb685d002c70
mdm-code <code.madamczyk@gmail.com>
2022-06-07 01:48:23 +0200

    [FEAT] Stubbed package documentation

--- doc.go
+++ doc.go
@@ -1,1 +1,32 @@
+/*
+Package prg2p implements a grapheme-to-phoneme rule-based converter.
+
+Usage
+	package main
+
+	import (
+		"fmt"
+
+		"github.com/mdm-code/prg2p"
+	)
+
+	func main() {
+		f, err := os.Open("static/rules.txt")
+		defer f.Close()
+		g2p, err := prg2p.Load(f)
+
+		var trans []string
+		for _, w := range []string{"ala", "ma", "kota"} {
+			t, err := g2p.Transcribe(w, false)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			trans = append(trans, t...)
+		}
+		for _, t := range trans {
+			fmt.Println(t)
+		}
+	}
+*/
 package prg2p
