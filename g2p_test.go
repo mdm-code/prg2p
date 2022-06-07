696a42127972e61b8ccaaefbd89c75b70ac3c8a4
mdm-code <code.madamczyk@gmail.com>
2022-06-07 14:31:42 +0200

    [FEAT] Added nil node tree check in Transcribe

--- g2p_test.go
+++ g2p_test.go
@@ -34,6 +34,15 @@ func TestLoadFails(t *testing.T) {
 	}
 }
 
+// Test empty Transcribe errors out when trie is nil
+func TestErrorTrieNil(t *testing.T) {
+	g2p := NewG2P(nil)
+	_, err := g2p.Transcribe("test", false)
+	if err == nil {
+		t.Error("nil tree should cause Transcribe to fail")
+	}
+}
+
 // Test G2P.Transcribe() package interface method.
 func TestTranscribe(t *testing.T) {
 	cases := []struct {
